052a9567654a1fc0c64c05aaee6213b8009a15af
Yevhenii Onipko <y.onipko@kyivstar.net>
2020-03-06 13:12:02 +0200

    disable preauth boolean

--- android/src/main/java/ua/kyivstar/sdk/portmone/rnecom/Constants.java
+++ android/src/main/java/ua/kyivstar/sdk/portmone/rnecom/Constants.java
@@ -4,6 +4,6 @@ public final class Constants {
     public final static int REQUEST_CODE = 123;
     public final static int CORNER_RADIUS = 100;
     public final static String BILL_NUMBER = "";
-    public final static boolean ALLOW_PRE_AUTH = true;
+    public final static boolean ALLOW_PRE_AUTH = false;
     public final static String ATTR_1 = "P";
 }
