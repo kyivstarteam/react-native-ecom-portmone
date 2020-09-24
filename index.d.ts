c39ce76329753ce80a5f1f974ac04b3bf1b9263f
Ruslan Yegorenko <ruslanyegorenko@MacBook-Pro-Ruslan.local>
2020-09-24 10:03:00 +0300

    feature/KME-4742-PortmoneSDK-addNewMethod add initCardPaymentByToken

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,12 @@
 declare module '@kyivstarteam/react-native-ecom-portmone' {
 
-    interface SavingCard {
+    export interface SavingCardParams {
         token: string
+    }
+
+    interface PaymentsByTokenParams {
+        token: string
+        cardMask: string
     }
 
     export type PaymentType = 'account' | 'phone' | undefined
@@ -9,14 +14,27 @@ declare module '@kyivstarteam/react-native-ecom-portmone' {
     class PortmoneCardModule {
         private lang: string;
         constructor(lang: string);
+
         initCardPayment(
             payeeId: string,
             phoneNumber: string,
             billAmount: number,
             uid: string,
             type?: PaymentType
+        ): Promise<PaymentsByTokenParams>;
+
+        initCardPaymentByToken(
+            payeeId: string,
+            phoneNumber: string,
+            billAmount: number,
+            uid: string,
+            cardMask: string,
+            token: string,
+            type?: PaymentType,
         ): Promise<void>;
-        initCardSaving(payeeId: string): Promise<SavingCard>;
+
+        initCardSaving(payeeId: string): Promise<SavingCardParams>;
     }
+
     export default PortmoneCardModule;
 }
